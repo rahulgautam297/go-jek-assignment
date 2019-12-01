dcea39b4b026417b4556e6b1e31e6b83f82185bf
rahul_gautam <rahulgautam297@gmail.com>
2019-12-01 17:44:14 +0530

    feat: add gemfile and parking lot file

---
+++ lib/parking_lot.rb
@@ -0,0 +1,15 @@
+class ParkingLot
+  PARKING_FULL_MESSAGE = 'Sorry, parking lot is full'
+  attr_accessor :slots, :size
+
+  def initialise(size)
+    @size = size
+    current_size = 0
+    @slots = []
+    size.to_i.times do |index|
+      slot_number = index + 1
+      @slots[index] = Slot.new(slot_number)
+    end
+    puts "Created a parking lot with #{size} slots"
+  end
+end
---
+++ specs/lib/parking_lot_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../lib/parking_lot'
+
+describe ParkingLot do
+  describe 'initialize a parking lot' do
+    context 'given input paramaters are correct' do
+      it 'returns new parking lot instance' do
+        parking_lot = ParkingLot.new('1')
+        expect(parking_lot.slots.length).to eql(1)
+        expect(parking_lot.slots[0].id).to eql(1)
+        parking_lot2 = ParkingLot.new(2)
+        expect(parking_lot2.slots.length).to eql(2)
+      end
+    end
+
+    context 'given input paramaters have partial or no parameters' do
+      it 'throws exception due to missing argument errors' do
+        expect { ParkingLot.new }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
